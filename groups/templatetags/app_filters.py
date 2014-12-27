1830f8295fb8f0f5c7aeb5e0335667c98ad325a9
Jiahao Li <isundaylee.reg@gmail.com>
2014-12-27 13:27:57 -0500

    Tweaks group show page interface.

---
+++ groups/templatetags/app_filters.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='dollar_amount')
+def dollar_amount(amount):
+  if amount >= 0:
+    return "$" + str(amount)
+  else:
+    return ("-$") + str(-amount)
