c46c9b105fbfe5c1799d462d15b299f93f3632bb
Jiahao Li <isundaylee.reg@gmail.com>
2014-12-27 15:29:18 -0500

    Heroku.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn splitit.wsgi --log-file -
--- splitit/settings.py
+++ splitit/settings.py
@@ -82,6 +82,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
@@ -91,5 +92,16 @@ STATICFILES_DIRS = (
 
 # Template settings
 TEMPLATE_LOADERS = (
-    'django.template.loaders.app_directories.Loader',
+  'django.template.loaders.app_directories.Loader',
 )
+
+# Heroku
+# Parse database configuration from $DATABASE_URL
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
